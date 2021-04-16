de39a0b4eb830dce0e0008f6afde59bd2d2dc85c
Antonio Artigas <edantonio.artigas@gmail.com>
2021-04-16 08:03:05 -0400

    Add test for simple POST request

--- spec/sample.spec.ts
+++ spec/sample.spec.ts
@@ -5,8 +5,10 @@ import fse from "fs-extra";
 import supertest from "supertest";
 
 import { Server } from "../src/server";
+import { Workout } from "../src/workout";
 
-// We're using the setup below to create consistent locations for different tests
+// We're using the setup below to create consistent locations for different tests and running
+// in parallel as well
 // https://stackoverflow.com/questions/41111620/testing-the-file-system-with-jest/49155837
 const DIR_BASE = path.resolve(__dirname, "__fixtures__");
 const EXAMPLE_WORKOUTS = path.resolve(__dirname, "example-workouts");
@@ -35,11 +37,46 @@ describe("GET /workout - List all workouts available", () => {
     const app = server.app;
 
     it("Should return 200 with all the workouts available", () => {
-        supertest(app)
-        .get("/workout")
-        .then(response => {
-            const workouts = server.serializer.listWorkoutNames();
-            expect(response.body).toEqual(workouts);
-        });
+        return supertest(app)
+            .get("/workout")
+            .expect(200)
+            .then(response => {
+                const workouts = server.serializer.listWorkoutNames();
+                expect(response.body).toEqual(workouts);
+            });
+    });
+});
+
+describe("POST /workout - Create a workout with the specified name", () => {
+    const DIR_ID = crypto
+        .createHash("md5")
+        .update("POST /workout/ - Create a workout with the specified name")
+        .digest("hex");
+
+    const DIR_PATH = path.resolve(DIR_BASE, `workout${DIR_ID}`);
+    fse.copySync(EXAMPLE_WORKOUTS, DIR_PATH);
+
+    const workout: Workout = {
+        name: "Test workout",
+        rounds: [
+            {
+                name: "Run around in a circle at 60 fps",
+                time: 1
+            }
+        ]
+    };
+
+    const server = new Server({ mainPath: DIR_PATH });
+    const app = server.app;
+
+    it("Should return 201 indicating the workout was created in the backed", () => {
+        return supertest(app)
+            .post("/workout")
+            .send(workout)
+            .expect(201)
+            .then(() => {
+                const workoutInBackend = server.serializer.read(workout.name);
+                expect(workoutInBackend).toEqual(workout);
+            });
     });
 });
